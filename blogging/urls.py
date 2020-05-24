da981497520453f3ac5dc4a9dd23811049e315d5
Philip Korte <kortephilip@gmail.com>
2020-05-24 14:51:38 -0700

    completed a post form

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blogging.views import list_view, detail_view  # stub_view
+from blogging.views import list_view, detail_view, add_model  # stub_view
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", list_view, name="blog_index"),
     # path('posts/<int:post_id>/', stub_view, name="blog_detail"),
     path("posts/<int:post_id>/", detail_view, name="blog_detail"),
+    path("add/", add_model, name="add_post"),
 ]
