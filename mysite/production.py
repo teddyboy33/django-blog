9ec95aaf13a3b06f032b27d8be8295286112d4df
Philip Korte <kortephilip@gmail.com>
2020-05-31 12:03:57 -0700

    added secret_key

--- mysite/production.py
+++ mysite/production.py
@@ -14,6 +14,7 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "localhost"]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "(q1_p3t-7%l)i(1$@#s3g)&#@4#9)0r@^()y+ip8ffa^ua#ia$"
 
 SITE_ID = 1
